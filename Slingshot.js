b4a39d88212463052f58146bc35688c99a057338
kalravm41 <bhattarchana50@github.com>
2020-06-11 18:31:26 +0530

    project32

---
+++ Slingshot.js
@@ -0,0 +1,31 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+    attach(Body){
+        this.sling.bodyA = Body;
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            fill("yellow")
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
